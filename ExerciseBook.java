aa81678f6965bba39655a9ca593c1511a74f84df
omelet1 <yojinwo11git@gmail.com>
2023-09-05 10:48:24 +0900

    프로그래머스 신설

---
+++ ExerciseBook.java
@@ -0,0 +1,17 @@
+public class ExerciseBook {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("아이티윌 테스트");
+         System.out.println("아이티윌 테스트2");
+           System.out.println("아이티윌 테스트3");
+           System.out.println("아이티윌 테스트 230821");
+    }
+
+
+
+}
+
+
+
+    
